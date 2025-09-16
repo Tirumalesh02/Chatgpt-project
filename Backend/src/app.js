033d49fa41136e680c217d135442be13091046c7
Thirumalesh <Tirumalesh02>
2025-09-16 14:20:45 +0530

    Updating Backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,7 @@ const path = require("path");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174",
+  "https://chatgpt-project-0vpi.onrender.com",
 ];
 
 app.use(cors({
